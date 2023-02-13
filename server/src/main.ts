f786d6269cbe1fdb94706b92254576675789f6fc
therealtoresto <therealtoresto@gmail.com>
2023-02-13 11:14:00 +0200

    added docker

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+  const config = await app.get(ConfigService);
+  const port = config.get<number>('API_PORT');
+  await app.listen(port || 3001, () =>
+    console.log(`Server is sterted on ${port}`),
+  );
 
   if (module.hot) {
     module.hot.accept();
--- server/src/modules/app.module.ts
+++ server/src/modules/app.module.ts
@@ -4,29 +4,40 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Bank } from '../models/bank.model';
 import { BankModule } from './bank.module';
-import { join } from 'path';
 import { Transaction } from '../models/transaction.model';
 import { Category } from '../models/category.model';
 import { CategoryModule } from './category.module';
 import { TransactionModule } from './transaction.module';
 import { StatisticsModule } from './statistics.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'fin_manager',
-      entities: [Bank, Transaction, Category],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('TYPEORM_HOST'),
+        port: parseInt(configService.get('TYPEORM_PORT')),
+        username: configService.get('TYPEORM_USERNAME'),
+        password: configService.get('TYPEORM_PASSWORD'),
+        database: configService.get('TYPEORM_DATABASE'),
+        // entities: [Bank, Transaction, Category],
+        entities: [__dirname + 'dist/**/*.model.ts'],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: true,
+      }),
     }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: './.env' }),
     BankModule,
     CategoryModule,
     TransactionModule,
